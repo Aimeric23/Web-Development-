948612ef6a97b02e93df6d562c98ce1b0014daea
Aimeric <atshibua@nyit.edu>
2019-07-26 21:49:29 -0400

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,50 @@
+function expandWithInfo()
+{
+	
+	
+}
+
+
+
+
+/*	var theImages=new Array();
+theImages[0]="Feed1.jpg";
+theImages[1]="Feed2.jpg";
+theImages[2]="Feed4.jpg";
+
+var j = 0;
+var p = theImages.length;
+
+var preBuffer = new Array();
+
+for (i = 0; i < p; i++)
+{
+   preBuffer[i] = new Image();
+   preBuffer[i].src = theImages[i];
+}
+
+var whichImage = Math.round(Math.random()*(p-1));
+
+function intialize()
+{
+	document.write('<img src="'+theImages[whichImage]+'">');
+}
+
+
+ document.addEventListener("click", function(event)
+  {
+    // Ignore right-clicks
+    if (event.button == 2)
+      return;
+
+    // Search the link associated with the click
+    var link = event.target;
+    while (link && !(link instanceof HTMLAnchorElement))
+      link = link.parentNode;
+
+    if (!link || link.protocol != "abp:")
+      return;
+
+    // This is our link - make sure the browser doesn't handle it
+    event.preventDefault();
+    event.stopPropagation();*/
